ce36f5b78f32b31225baf04cfd81dab8a0cc54a9
welcome11252umn <masonhaake@icloud.com>
2025-06-06 12:57:42 -0400

    Delete vizio-browser directory

--- vizio-browser/scripts/main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("menuToggle").addEventListener("click", () => {
-  const menu = document.getElementById("sideMenu");
-  menu.classList.toggle("hidden");
-});
-
-function navigateTo(page) {
-  document.getElementById("mainFrame").src = page;
-}
